3a14b6ba8994a15ce57f116000ed04212f81459c
JaccobStanton <jstanton23@my.stlcc.edu>
2025-09-03 14:30:06 -0500

    Contained all components now into centralized App location for proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import Hero from "./components/Hero/Hero";
+import FieldScroller from "./components/FieldScroller";
+import TechShowcase from "./components/TechShowcase";
+import DataFlowPath from "./components/DataFlowPath";
+import FitMatrix from "./components/FitMatrix";
+import BlackIntroSection from "./components/blackIntroSection";
+import { FooterCTA } from "./components/FooterCTA";
 
 export default function App() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <BlackIntroSection id="next-section" />
+      <FieldScroller id="after-black-intro" />
+      <TechShowcase />
+      <DataFlowPath />
+      <FitMatrix />
+      <FooterCTA />
+    </>
+  );
 }
