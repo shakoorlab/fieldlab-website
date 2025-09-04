70e5e5f78de40d63d8fa5da61e651babafcfbf08
JaccobStanton <jstanton23@my.stlcc.edu>
2025-09-04 13:45:21 -0500

    Added react routing to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Hero from "./components/Hero/Hero";
 import FieldScroller from "./components/FieldScroller";
 import TechShowcase from "./components/TechShowcase";
@@ -5,8 +7,11 @@ import DataFlowPath from "./components/DataFlowPath";
 import FitMatrix from "./components/FitMatrix";
 import BlackIntroSection from "./components/blackIntroSection";
 import { FooterCTA } from "./components/FooterCTA";
+import ClimateSmart from "./components/Fields/Climate_Smart/ClimateSmart";
+import HPI from "./components/Fields/HPI/HPI";
 
-export default function App() {
+// Home route composed of your existing sections
+function Home() {
   return (
     <>
       <Hero />
@@ -17,5 +22,26 @@ export default function App() {
       <FitMatrix />
       <FooterCTA />
     </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fields/hpi" element={<HPI />} />
+        <Route path="/fields/climate-smart" element={<ClimateSmart />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: 32 }}>
+              <h1>Not Found</h1>
+              <p>That page doesn’t exist.</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
